3380743f1ce5cb5996addb5a29af9c7eca1a4ce2
Karanei-Kimutai <168116985+Karanei-Kimutai@users.noreply.github.com>
2024-06-11 11:53:02 +0300

    Add files via upload

--- Exercise9.c
+++ Exercise9.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-struct student_data{
+/*struct student_data{
     char name[50];
     int age;
     int student_id;
@@ -15,6 +15,41 @@ int main(){
     printf("Enter student id number:");
     scanf("%d",&student1.name);
 
+
+
+
+
+    return 0;
+}*/
+struct adress{
+    char Area[20];
+    char City[20];
+};
+struct student_information{
+    char name[50];
+    int age;
+    int student_id;
+    struct adress residential_adress;
+    char employment_status[20];
+};
+
+int main(){
+    struct student_information student1;
+    printf("Enter name:");
+    scanf("%s",student1.name);
+    printf("\nEnter age:");
+    scanf("%d",&student1.age);
+    printf("\nEnter student id:");
+    scanf("%d",&student1.student_id);
+    printf("\nEmployment status:");
+    scanf("%s",student1.employment_status);
+    printf("\nEnter Area Adress:");
+    scanf("%s", student1.residential_adress.Area);
+    printf("\nEnter City:");
+    scanf("%s",student1.residential_adress.City);
+    
+    
+    
 
 
 
